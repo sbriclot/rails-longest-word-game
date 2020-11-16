5ff2fe859ae985453595c7d8ff007311ddfd6a44
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-16 22:22:20 +0100

    Longest word code cleaning

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,44 +3,39 @@ require 'open-uri'
 class GamesController < ApplicationController
   def new
     alphabet = ("A".."Z").to_a
+    vowels = ["A", "E", "I", "O", "U"]
     @letters = [];
-    10.times { @letters << alphabet.sample }
+    7.times { @letters << alphabet.sample }
+    3.times { @letters << vowels.sample }
+    @letters.shuffle
   end
 
   def score
     @proposal = params[:proposal].upcase
     ary_letters = params[:letters].split
     @letters = ary_letters.join(", ")
-    unless use_grid_letters?(ary_letters, @proposal)
-      @result = "NotInLetters"
-    else
-      @result = word_in_dictionnary?(@proposal)
-      if @result == "Win"
-        @points = @proposal.length
-        save_score(get_score + @points)
-      end
+    @use_grid_only =  use_grid_letters?(ary_letters, @proposal)
+    @valid_word = word_in_dictionnary?(@proposal)
+    if @valid_word
+      @points = @proposal.length
+      save_score(get_score + @points)
     end
     @total_score = get_score
-    
   end
-  
+
   private
 
   def use_grid_letters?(letters, proposal)
     proposal.upcase.split("") do |letter|
       index = letters.index(letter)
       return false unless index
-  
+
       letters.delete_at(index)
     end
     true
   end
 
   def word_in_dictionnary?(proposal)
-    if JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@proposal}").read)["found"]
-      return "Win"
-    else
-      return "InvalidWord"
-    end
+    return JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@proposal}").read)["found"]
   end
 end
